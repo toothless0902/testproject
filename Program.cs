be53419c4379ee8e268d092126f1c91dfa05e2a2
시형 박 <parksh5317@gmail.com>
2026-03-20 11:22:13 +0900

    feat: 퀀트 봇 저가 매수 알고리즘 구현
    -10개 종목 가격 배열(Araay) 생성
    -Array.Sort를 활용한 가격 순 정렬 로직 추가
    -while 문으로 입금액 부족 시 반복 입금 기능 구현

--- Program.cs
+++ Program.cs
@@ -22,7 +22,7 @@ class Program
         Console.WriteLine("최종 합계: " + (money + bonus) + "원");
         Console.WriteLine("-------------------------------");
 
-        while (money <= 500000)
+        while (money < 500000)
         {
             Console.WriteLine("퀀트 봇을 실행하기 위한 최소 금액이 부족합니다");
             Console.WriteLine("추가 입금 금액을 입력해주세요: " + "현재 잔액 :" + money);
@@ -43,29 +43,48 @@ class Program
                 Console.WriteLine("더 많은 금액을 예치하세요");
             }
         }
+        
         int buycount = 0;
+        int[] prices = { 88000, 12000, 45000, 32000, 900000, 500000, 3000, 20190 };
+
+        Console.WriteLine("\n[알고리즘 가동] 시장 데이터 분석 중...");
+
+        Array.Sort(prices);
      
-        for( int i = 1; i <= 10; i++)
+        for( int i = 0; i < prices.Length; i++) //prices.length를 활용한 반복 -> 리스트 안의 인덱스 값 수  ex) 인덱스 총 8개 -> prices.Length = 8
         {
-            if (buycount < 5)
+            if (buycount < 5 && money >= prices[i])
             {
 
 
-                money -= 100000;
+
+                money -= prices[i]; //money = money - prices[i] -> prices[i]는 인덱스 몇번째의 수
                 buycount++;
-                Console.WriteLine(i + "번째 종목 분석 중...");
-                Console.WriteLine(i + "번째 매수 완료. 현재 잔액 : " + money);
+
+
+                Console.WriteLine($"{i + 1}번째 후보 분석 : 가격 {prices[i]}원");
+                Console.WriteLine($"{buycount+1}번째 매수 완료! 현재 잔액 {money}원");
 
             }
-            else
+            else if (buycount >= 5)
             {
-                Console.Write("목표 수량 5개를 다 채웠으. 분석 종료");
+                Console.WriteLine("\n[분석 종료] 목표 수량 5개를 모두 채웠습니다.");
                 break;
+            }
+            else if (money < prices[i])
+            {
+                Console.WriteLine($"{i + 1} 번째 후보 분석 : 가격 {prices[i]}원 -> 잔액 부족으로 맷 불가!");
             }
 
         }
 
-       
+        Console.WriteLine("\n----------------------------------");
+        Console.WriteLine($"{name}님의 퀀트 봇 운용 결과");
+        Console.WriteLine($"총 매수 종목 수: {buycount+1}개");
+        Console.WriteLine($"최종 잔액: {money}원");
+        Console.WriteLine("\n-----------------------------------");
+
+
 
   
 
